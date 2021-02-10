1baa85b6039cf0115187b18d1340099fc8a77e17
ro-scsds <ro@scsds.com>
2021-02-09 22:15:57 -0800

    added dash

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { withAuthenticator } from 'aws-amplify-react'
 import Amplify, { Auth } from 'aws-amplify';
 import aws_exports from './aws-exports';
+
+
+
 Amplify.configure(aws_exports);
 
 class App extends Component {
@@ -11,18 +13,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+          <iframe width="1020" height="800" src="https://app.powerbi.com/view?r=eyJrIjoiYjM2ZjI4YmUtZWE2YS00ZmQ5LWJiMGUtNjI0MzJkNGI5NzgyIiwidCI6ImJhZmIzMmE2LTRjYzUtNDgwMy04ZmQ3LTY1NWUwMzMzMzM0NyIsImMiOjZ9" frameborder="0" allowFullScreen="true"></iframe>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          
         </header>
       </div>
     );
